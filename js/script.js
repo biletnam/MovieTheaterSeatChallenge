824bd4dc566defdcac8e992c9e543e855e0d14f1
Landall Proctor <landallproctor@gmail.com>
2015-09-04 15:37:43 -0400

    Created button with jQuery dropdown.

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// create some buttons that open forms
+
+
+jQuery(document).ready(function(){
+
+var $ = jQuery;
+
+$('.dropdown-toggle').click(function() {
+
+	if($('.dropdown-menu').css('display') == 'none'){
+	$('.dropdown-menu').slideDown(250);
+}
+else {
+	$('.dropdown-menu').slideUp(250);
+}
+})
+})
